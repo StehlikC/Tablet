4692df0a7c33aaf53f885bd7a25ef440ced4f138
Casey Stehlik <casey.stehlik@stehlikc.net>
2015-06-28 17:54:32 -0500

    Initial Commit

---
+++ Tablet.Tests/TabletTests.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions.TestingHelpers;
+using Xunit;
+
+namespace Tablet.Tests
+{
+    public class TabletTests
+    {
+        [Fact]
+        public void Init_GivenNoDirectory_CreatesFileStructureInCurrentDirectory()
+        {
+            var mock = new MockFileSystem();
+
+            mock.Directory.SetCurrentDirectory(@"C:\Temp");
+
+            var sut = new Tablet(mock);
+
+            sut.Init();
+
+            Assert.True(mock.Directory.Exists(@"C:\Temp\.tablet"), "Directory does not exist.");
+            Assert.True(mock.Directory.Exists(@"C:\Temp\.tablet\objects"), "Directory does not exist.");
+            Assert.True(mock.Directory.Exists(@"C:\Temp\.tablet\sets"), "Directory does not exist.");
+        }
+
+        [Fact]
+        public void Init_GivenDirectory_CreatesFileStructureInThatDirectory()
+        {
+            var mock = new MockFileSystem();
+            
+            var sut = new Tablet(@"C:\Temp", mock);
+
+            sut.Init();
+
+            Assert.True(mock.Directory.Exists(@"C:\Temp\.tablet"), "Directory does not exist.");
+            Assert.True(mock.Directory.Exists(@"C:\Temp\.tablet\objects"), "Directory does not exist.");
+            Assert.True(mock.Directory.Exists(@"C:\Temp\.tablet\sets"), "Directory does not exist.");
+        }
+
+        [Fact]
+        public void Init_WhenFileStructureAlreadyExists_ThrowsException()
+        {
+            var mock = new MockFileSystem();
+
+            mock.Directory.CreateDirectory(@"C:\Temp\.tablet");
+
+            var sut = new Tablet(@"C:\Temp", mock);
+
+            Assert.Throws<TabletAlreadyInitializedException>(() => sut.Init());
+        }
+
+        [Fact]
+        public void HashObject_StoresObjectInObjectsDirectory_ReturnsTheHash()
+        {
+            var mock = new MockFileSystem();
+
+            mock.Directory.SetCurrentDirectory(@"C:\Temp");
+
+            var sut = new Tablet(mock);
+
+            sut.Init();
+
+            var actual = sut.HashObject<int>(1);
+
+            Assert.Equal("356a192b7913b04c54574d18c28d46e6395428ab", actual);
+            Assert.True(mock.File.Exists(@"C:\Temp\.tablet\objects\35\6a192b7913b04c54574d18c28d46e6395428ab"), "File does not exist.");
+        }
+
+        [Fact]
+        public void HashObject_StoresCompressedDataInObjectsDirectory_ReturnsTheHash()
+        {
+            var mock = new MockFileSystem();
+
+            mock.Directory.SetCurrentDirectory(@"C:\Temp");
+
+            var sut = new Tablet(mock);
+
+            sut.Init();
+
+            var actual = sut.HashObject<int>(1);
+
+            Assert.Equal("356a192b7913b04c54574d18c28d46e6395428ab", actual);
+            Assert.Equal("9996100969624815432264886412158224202226146212926120718818993514401231101248998781054237136199130", String.Join("", mock.File.ReadAllBytes(@"C:\Temp\.tablet\objects\35\6a192b7913b04c54574d18c28d46e6395428ab")));
+        }
+
+        [Fact]
+        public void HashObjectWithKey_StoresCompressedDataInKeyedObjectDirectory_ReturnsTheHash()
+        {
+            var mock = new MockFileSystem();
+
+            mock.Directory.SetCurrentDirectory(@"C:\Temp");
+
+            var sut = new Tablet(mock);
+
+            sut.Init();
+
+            var actual = sut.HashObjectWithKey<Fake, string>(new Fake { Value = 1 }, k => Convert.ToString(k.Value));
+
+            Assert.Equal("356a192b7913b04c54574d18c28d46e6395428ab", actual);
+            Assert.True(mock.File.Exists(@"C:\Temp\.tablet\sets\35\6a192b7913b04c54574d18c28d46e6395428ab"), "File does not exist.");
+        }
+
+        [Fact]
+        public void GetObjectSet_ReturnsEnumerableObjects()
+        {
+            var mock = new MockFileSystem();
+
+            mock.Directory.SetCurrentDirectory(@"C:\Temp");
+
+            var sut = new Tablet(mock);
+
+            sut.Init();
+
+            sut.HashObjectWithKey<Fake, string>(new Fake { Value = 1 }, k => Convert.ToString(k.Value));
+
+            var actual = sut.GetObjectSet<Fake, int>(1);
+
+            Assert.Equal(new List<Fake> { new Fake { Value = 1 } }, actual, new FakeComparer());
+        }
+
+        [Fact]
+        public void GetObjectSet_WhenCalledTwice_ReturnsEnumerableBothObjects()
+        {
+            var mock = new MockFileSystem();
+
+            mock.Directory.SetCurrentDirectory(@"C:\Temp");
+
+            var sut = new Tablet(mock);
+
+            sut.Init();
+
+            sut.HashObjectWithKey<Fake, string>(new Fake { Value = 1 }, k => Convert.ToString(k.Value));
+            sut.HashObjectWithKey<Fake, string>(new Fake { Value = 1 }, k => Convert.ToString(k.Value));
+
+            var actual = sut.GetObjectSet<Fake, int>(1);
+
+            Assert.Equal(new List<Fake> { new Fake { Value = 1 }, new Fake { Value = 1 } }, actual, new FakeComparer());
+        }
+
+        [Fact]
+        public void HashObjectLoadTest()
+        {
+            if (Directory.Exists(@"C:\Temp"))
+                Directory.Delete(@"C:\Temp", true);
+
+            var sut = new Tablet(@"C:\Temp");
+
+            sut.Init();
+
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                sut.HashObjectWithKey<Fake, string>(new Fake { Value = 1 }, k => Convert.ToString(k.Value));
+            }
+
+            watch.Stop();
+
+            Assert.True(watch.Elapsed < TimeSpan.FromSeconds(1));
+
+            //if (Directory.Exists(@"C:\Temp"))
+                //Directory.Delete(@"C:\Temp", true);
+        }
+    }
+
+    [Serializable]
+    public class Fake
+    {
+        public int Value { get; set; }
+    }
+
+    public class FakeComparer : IEqualityComparer<Fake>
+    {
+        public bool Equals(Fake x, Fake y)
+        {
+            return x.Value == y.Value;
+        }
+
+        public int GetHashCode(Fake obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
---
+++ Tablet/Tablet.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.IO.Abstractions;
+using System.Security.Cryptography;
+using System.Text;
+using System.Linq;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO.Compression;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Tablet
+{
+    public class Tablet
+    {
+        private const string Directory = ".tablet";
+        private const string ObjectsDirectory = "objects";
+        private const string SetsDirectory = "sets";
+
+        private readonly IFileSystem _fileSystem;
+        private string _root;
+
+        public Tablet()
+        {
+            _fileSystem = new FileSystem();
+            _root = _fileSystem.Directory.GetCurrentDirectory();
+        }
+
+        public Tablet(IFileSystem fileSystem)
+        {
+            _fileSystem = fileSystem;
+            _root = _fileSystem.Directory.GetCurrentDirectory();
+        }
+
+        public Tablet(string root)
+        {
+            _fileSystem = new FileSystem();
+            _root = root;
+        }
+
+        public Tablet(string root, IFileSystem fileSystem)
+        {
+            _root = root;
+            _fileSystem = fileSystem;
+        }
+
+        public void Init()
+        {
+            if (!_fileSystem.Directory.Exists(String.Join(@"\", _root, Directory)))
+            {
+                _fileSystem.Directory.CreateDirectory(String.Join(@"\", _root, Directory));
+                _fileSystem.Directory.CreateDirectory(String.Join(@"\", _root, Directory, ObjectsDirectory));
+                _fileSystem.Directory.CreateDirectory(String.Join(@"\", _root, Directory, SetsDirectory));
+            }
+            else
+            {
+                throw new TabletAlreadyInitializedException();
+            }
+        }
+
+        public string HashObject<T>(T obj)
+        {
+            var hash = BitConverter.ToString(SHA1.Create().ComputeHash(Encoding.UTF8.GetBytes(obj.ToString()))).Replace("-", "").ToLower();
+
+            if (!_fileSystem.Directory.Exists(String.Join(@"\", _root, Directory, ObjectsDirectory, hash.ToCharArray().Take(2))))
+            {
+                _fileSystem.Directory.CreateDirectory(String.Join(@"\", _root, Directory, ObjectsDirectory, String.Join("", hash.ToCharArray().Take(2))));
+
+                using (var file = _fileSystem.File.Create(String.Join(@"\", _root, Directory, ObjectsDirectory, String.Join("", hash.ToCharArray().Take(2)), String.Join("", hash.ToCharArray().Skip(2).Take(38)))))
+                {
+                    using (var stream = new MemoryStream())
+                    {
+                        var bf = new BinaryFormatter();
+
+                        using (var compression = new DeflateStream(stream, CompressionMode.Compress, true))
+                        {
+                            bf.Serialize(compression, obj);
+                        }
+
+                        stream.WriteTo(file);
+                    }
+                }
+            }
+
+            return hash;
+        }
+
+        public string HashObjectWithKey<T, TProperty>(T obj, Func<T, TProperty> selector)
+        {
+            var hash = BitConverter.ToString(SHA1.Create().ComputeHash(Encoding.UTF8.GetBytes(Convert.ToString(selector.Invoke(obj))))).Replace("-", "").ToLower();
+
+            using (var os = new MemoryStream())
+            {
+                var bf = new BinaryFormatter();
+
+                using (var ds = new DeflateStream(os, CompressionMode.Compress, true))
+                {
+                    var set = GetObjectSet<T, TProperty>(selector.Invoke(obj));
+
+                    set.Add(obj);
+
+                    bf.Serialize(ds, set);
+                }
+
+                if (!_fileSystem.Directory.Exists(String.Join(@"\", _root, Directory, SetsDirectory, hash.ToCharArray().Take(2))))
+                {
+                    _fileSystem.Directory.CreateDirectory(String.Join(@"\", _root, Directory, SetsDirectory, String.Join("", hash.ToCharArray().Take(2))));
+
+                    using (var fs = _fileSystem.File.Open(String.Join(@"\", _root, Directory, SetsDirectory, String.Join("", hash.ToCharArray().Take(2)), String.Join("", hash.ToCharArray().Skip(2).Take(38))), FileMode.Create))
+                    {
+                        os.WriteTo(fs);
+                    }
+                }
+            }
+
+            return hash;
+        }
+
+        public IList<T> GetObjectSet<T, TProperty>(TProperty key)
+        {
+            var hash = BitConverter.ToString(SHA1.Create().ComputeHash(Encoding.UTF8.GetBytes(Convert.ToString(key)))).Replace("-", "").ToLower();
+
+            try
+            {
+                using (var fs = _fileSystem.File.Open(String.Join(@"\", _root, Directory, SetsDirectory, String.Join("", hash.ToCharArray().Take(2)), String.Join("", hash.ToCharArray().Skip(2).Take(38))), FileMode.Open, FileAccess.Read))
+                {
+                    using (var os = new MemoryStream())
+                    {
+                        var bf = new BinaryFormatter();
+
+                        using (var ds = new DeflateStream(fs, CompressionMode.Decompress, true))
+                        {
+                            ds.CopyTo(os);
+                        }
+
+                        os.Position = 0;
+
+                        if (os.Length == 0)
+                        {
+                            return new List<T>();
+                        }
+
+                        return (List<T>)bf.Deserialize(os);
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                return new List<T>();
+            }
+            catch(DirectoryNotFoundException)
+            {
+                return new List<T>();
+            }
+        }
+    }
+}
